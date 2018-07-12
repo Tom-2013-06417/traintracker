a38aa7b62d65211282fdabced32ddcf3225ee482
Tom <airbreather7@gmail.com>
2018-07-13 01:42:20 +0800

    Modify project directory structure

---
+++ src/layouts/Base.vue
@@ -0,0 +1,80 @@
+<template>
+  <v-app>
+    <v-navigation-drawer
+      v-model="drawer"
+      persistent
+      enable-resize-watcher
+      app
+    >
+      <v-list>
+        <v-list-tile
+          value="true"
+          v-for="(item, i) in items"
+          :key="i"
+        >
+          <v-list-tile-action>
+            <v-icon v-html="item.icon"></v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="item.title"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar
+      app
+    >
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title v-text="title"></v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-toolbar>
+    <v-content>
+      <router-view/>
+    </v-content>
+    <v-navigation-drawer
+      temporary
+      :right="right"
+      v-model="rightDrawer"
+      app
+    >
+      <v-list>
+        <v-list-tile @click="right = !right">
+          <v-list-tile-action>
+            <v-icon>compare_arrows</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-footer app>
+      <span>&copy; 2017</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    drawer: true,
+    items: [
+      {
+        icon: 'home',
+        title: 'Overview'
+      },
+      {
+        icon: 'map',
+        title: 'Live Map'
+      },
+      {
+        icon: 'schedule',
+        title: 'Schedule'
+      }
+    ],
+    right: true,
+    title: 'TrainTracker'
+  }),
+  props: {
+    source: String
+  }
+}
+</script>
--- src/router.js
+++ src/router.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Base from './layouts/Base.vue'
+import BaseLite from './layouts/BaseLite.vue'
+import BaseAdmin from './layouts/BaseAdmin.vue'
 import Home from './views/Home.vue'
 import About from './views/About.vue'
 
@@ -9,13 +12,25 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      component: Base,
+      children: [
+        {
+          path: '',
+          component: Home
+        },
+        {
+          path: 'about',
+          component: About
+        }
+      ]
     },
     {
-      path: '/about',
-      name: 'about',
-      component: About
+      path: '/login',
+      component: BaseLite
+    },
+    {
+      path: '/admin',
+      component: BaseAdmin
     }
   ]
 })
