4ffc25b5e8e1878aff14714453f93c2b93f527d9
Tom <airbreather7@gmail.com>
2018-07-14 00:12:25 +0800

    Make base files have orange icons

--- src/layouts/Base.vue
+++ src/layouts/Base.vue
@@ -2,21 +2,38 @@
   <v-app>
     <v-navigation-drawer
       v-model="drawer"
-      persistent
+      permanent
       enable-resize-watcher
+      :mini-variant.sync="drawer"
       app
     >
+      <v-toolbar flat class="transparent">
+        <v-list class="pa-0">
+          <v-list-tile avatar>
+            <v-list-tile-avatar>
+              <img src="@/assets/img/train.svg" >
+            </v-list-tile-avatar>
+
+            <v-list-tile-content>
+              <v-list-tile-title>{{ title }}</v-list-tile-title>
+            </v-list-tile-content>
+
+          </v-list-tile>
+        </v-list>
+      </v-toolbar>
+      <v-divider></v-divider>
       <v-list>
         <v-list-tile
+          class="py-2"
           value="true"
           v-for="(item, i) in items"
           :key="i"
         >
           <v-list-tile-action>
-            <v-icon v-html="item.icon"></v-icon>
+            <v-icon medium color="orange accent-3" v-html="item.icon"></v-icon>
           </v-list-tile-action>
           <v-list-tile-content>
-            <v-list-tile-title v-text="item.title"></v-list-tile-title>
+            <v-list-tile-title class="black--text body-1" v-text="item.title"></v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
       </v-list>
@@ -25,13 +42,12 @@
       app
     >
       <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-toolbar-title v-text="title"></v-toolbar-title>
       <v-spacer></v-spacer>
     </v-toolbar>
     <v-content>
       <router-view/>
     </v-content>
-    <v-footer app>
+    <v-footer class="px-2" app>
       <span>&copy; 2017</span>
     </v-footer>
   </v-app>
