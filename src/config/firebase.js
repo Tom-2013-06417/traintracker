b4b480b064608fa92ada46fff227cfa38891dc09
Paulo Pascua <paulocpascua@gmail.com>
2018-07-14 00:16:00 +0800

    Add Firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyD676asiVdwmk1v7gPZifGQ1BEsBIm9e_Y",
+  authDomain: "hackatren-traintracker.firebaseapp.com",
+  databaseURL: "https://hackatren-traintracker.firebaseio.com",
+  projectId: "hackatren-traintracker",
+  storageBucket: "",
+  messagingSenderId: "81106495905"
+};
--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@ import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
+import firebase from 'firebase'
+import { firebaseConfig } from './config/firebase'
 import { store } from './store'
 import './registerServiceWorker'
+
+firebase.initializeApp(firebaseConfig)
+const db = firebase.database()
 
 Vue.config.productionTip = false
 
