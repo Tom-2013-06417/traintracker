1fe3cd53f0a8352db766e632835ce51555032ed7
Paulo Pascua <paulocpascua@gmail.com>
2018-07-14 14:19:34 +0800

    Add Leaflet map to Live Map view

--- src/views/LiveMap.vue
+++ src/views/LiveMap.vue
@@ -1,11 +1,41 @@
 <template>
   <v-container fluid grid-list-xs pa-0>
-    Pascua put some stuff here
+    <div id="map"></div>
   </v-container>
 </template>
 
 <script>
-export default {
+import L from 'leaflet'
 
+export default {
+  data () {
+    return {
+      map: null,
+      tileLayer: null,
+      layers: []
+    }
+  },
+  mounted () {
+    this.initMap()
+  },
+  methods: {
+    initMap () {
+      const center = [14.6054402, 120.982061]
+      const osmUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+      const osmAttrib = 'Map data &copy; <a href="https://openstreetmap.org">OpenStreetMap</a> contributors'
+      const tileLayer = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 12, attribution: osmAttrib})
+      const marker = L.marker(center).bindPopup('Hello, world! Doroteo Jose reppin\'').openPopup()
+
+      let map = L.map('map').setView(center, 12)
+      tileLayer.addTo(map)
+      marker.addTo(map)
+    }
+  }
 }
 </script>
+
+<style lang="less" scoped>
+#map {
+  height: 100vh;
+}
+</style>
