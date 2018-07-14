b6f8ee3dda4c8091cbdd36490e00a76f8f295eee
jaimee07 <jaimelyn.20@gmail.com>
2018-07-14 17:03:58 +0800

    Display fare for beep and single journey

--- src/views/PassengerSelect.vue
+++ src/views/PassengerSelect.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container fluid grid-list-xs pa-0>
     <v-select
-      v-model="starting_point"
+      v-model="startStation"
       :items="stations"
       item-text="name"
       item-value="id"
@@ -11,7 +11,7 @@
       persistent-hint
     ></v-select>
     <v-select
-      v-model="destination"
+      v-model="endStation"
       :items="stations"
       item-text="name"
       item-value="id"
@@ -20,9 +20,9 @@
       solo
       persistent-hint
     ></v-select>
-    Price for beep card: <h3>{{ beep_card_price }} {{ starting_point }}</h3>
+    Price for beep card: <h3>{{ getBeepFare(startStation, endStation) }}</h3>
     <br/>
-    Price for single ticket journey: <h3>{{ single_journey_price }} {{ destination }}</h3>
+    Price for single ticket journey: <h3>{{ getSingleJourneyFare(startStation, endStation) }}</h3>
     <br/>
     <v-btn color="green accent-3" large>Let's Go!</v-btn>
   </v-container>
@@ -34,6 +34,7 @@ const stations = [
   {
     id: 'roosevelt',
     name: 'Roosevelt',
+    order: 0,
     coordinates: {
       lat: 0,
       long: 0
@@ -42,6 +43,7 @@ const stations = [
   {
     id: 'balintawak',
     name: 'Balintawak',
+    order: 1,
     coordinates: {
       lat: 0,
       long: 0
@@ -50,6 +52,7 @@ const stations = [
   {
     id: 'monumento',
     name: 'Monumento',
+    order: 2,
     coordinates: {
       lat: 0,
       long: 0
@@ -58,6 +61,7 @@ const stations = [
   {
     id: 'fifth_avenue',
     name: '5th Avenue',
+    order: 3,
     coordinates: {
       lat: 0,
       long: 0
@@ -65,13 +69,47 @@ const stations = [
   }
 ]
 
+const fareMatrixForBeep = [
+  [11, 13, 15, 16],
+  [13, 11, 13, 15],
+  [15, 13, 11, 12],
+  [16, 15, 12, 11]
+]
+
+const fareMatrixForSingleJourney = [
+  [0, 15, 15, 20],
+  [15, 0, 15, 15],
+  [15, 15, 0, 15],
+  [20, 15, 15, 0]
+]
+
+const getBeepFare = (startStation, endStation) => {
+  if (!startStation || !endStation) {
+    return
+  }
+  const startStationOrder = stations.find(station => { return station.id === startStation }).order
+  const endStationOrder = stations.find(station => { return station.id === endStation }).order
+  return fareMatrixForBeep[startStationOrder][endStationOrder]
+}
+
+const getSingleJourneyFare = (startStation, endStation) => {
+  if (!startStation || !endStation) {
+    return
+  }
+  const startStationOrder = stations.find(station => { return station.id === startStation }).order
+  const endStationOrder = stations.find(station => { return station.id === endStation }).order
+  return fareMatrixForSingleJourney[startStationOrder][endStationOrder]
+}
+
 export default {
   data: () => ({
     stations: stations,
-    beep_card_price: 24,
-    single_journey_price: 30,
-    starting_point: '',
-    destination: ''
-  })
+    startStation: '',
+    endStation: ''
+  }),
+  methods: {
+    getBeepFare,
+    getSingleJourneyFare
+  }
 }
 </script>
