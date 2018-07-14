4031f0f22f6a7aefb27af5b284a378cbcc2775c6
Paulo Pascua <paulocpascua@gmail.com>
2018-07-14 22:51:45 +0800

    Initialize multiple train markers on load

--- src/api/index.js
+++ src/api/index.js
@@ -82,6 +82,62 @@ export const trains = [
       body_material: 'BI sheet',
       ventilation: 'Forced ventilation'
     }
+  },
+  {
+    id: 4,
+    max_capacity: 3000,
+    current_capacity: 0,
+    status: 'OK',
+    operating: false,
+    coordinates: [0, 0],
+    model: {
+      name: '1000 series',
+      year: 1984,
+      manufacturer: 'SA Ateliers de Constructions Electriques de Charleroi',
+      car_type: '8-axle (4-bogie) rigid body',
+      length: 29280,
+      width: 2500,
+      height: 3535,
+      configuration: 'MC-MC 59590 mm (2-car)',
+      articulation: 'double',
+      doors: {
+        width: 2500,
+        type: 'plug',
+        doors_per_side: 5
+      },
+      traction_system: 'DC 218 Kw Chopper Type x 1',
+      traction_power: '750 V DC OCS; pantograph power connection',
+      body_material: 'BI sheet',
+      ventilation: 'Forced ventilation'
+    }
+  },
+  {
+    id: 5,
+    max_capacity: 3000,
+    current_capacity: 1700,
+    status: 'OK', // OOS, DEC
+    operating: true,
+    coordinates: [14.5763261, 120.985999],
+    model: {
+      name: '1000 series',
+      year: 1984,
+      manufacturer: 'SA Ateliers de Constructions Electriques de Charleroi',
+      car_type: '8-axle (4-bogie) rigid body',
+      length: 29280,
+      width: 2500,
+      height: 3535,
+      configuration: 'MC-MC 59590 mm (2-car)',
+      articulation: 'double',
+      doors: {
+        width: 2500,
+        type: 'plug',
+        doors_per_side: 5
+      },
+      traction_system: 'DC 218 Kw Chopper Type x 1',
+      traction_power: '750 V DC OCS; pantograph power connection',
+      body_material: 'BI sheet',
+      ventilation: 'Forced ventilation'
+    }
   }
 ]
 
--- src/components/MapTrain.vue
+++ src/components/MapTrain.vue
@@ -8,23 +8,24 @@ import { fetchTrains, fetchCoordinates } from '@/api'
 
 const state = {
   map: null,
-  marker: null,
+  // marker: null,
   tileLayer: null,
-  layers: []
+  layers: [],
+  activeTrains: [],
+  markers: []
 }
 
 export default {
   data () {
     return state
   },
-  beforeMount () {
-    fetchTrains().then((response) => {
-      this.trains = response
-    })
-  },
   mounted () {
-    this.initMap()
-    this.updateCoordinates()
+    fetchTrains().then((response) => {
+      const trains = response
+      this.activeTrains = trains.filter(train => train.status === 'OK' && train.operating)
+      this.initMap()
+    })
+    // this.updateCoordinates()
   },
   methods: {
     initMap () {
@@ -32,11 +33,25 @@ export default {
       const osmUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
       const osmAttrib = 'Map data &copy; <a href="https://openstreetmap.org">OpenStreetMap</a> contributors'
       const tileLayer = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 16, attribution: osmAttrib})
-      this.marker = L.marker(center).bindPopup('Hello, world! Doroteo Jose reppin\'').openPopup()
 
       let map = L.map('map').setView(center, 12)
       tileLayer.addTo(map)
-      this.marker.addTo(map)
+      this.markers = this.getTrainMarkers()
+      this.markers.forEach(function (marker) {
+        marker.markerObj.addTo(map).bindPopup(marker.popupText, { autoClose: false }).openPopup()
+      })
+    },
+    getTrainMarkers () {
+      let markerInfo = []
+      if (this.activeTrains && this.activeTrains.length) {
+        this.activeTrains.forEach(function (train, index) {
+          markerInfo[index] = {
+            markerObj: L.marker(train.coordinates),
+            popupText: train.id.toString()
+          }
+        })
+      }
+      return markerInfo
     },
     updateCoordinates () {
       setInterval(() => {
